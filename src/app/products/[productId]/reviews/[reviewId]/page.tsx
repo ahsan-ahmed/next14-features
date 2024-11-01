debc5a64b2cdb1e7fb4ae3655a47ed20889ab922
ahsan-ahmed <ahsanahmed0124@gmail.com>
2024-11-01 17:27:09 +0500

    Implement page not found by routing and call not found function

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
 // Nested Dynamic Routes
 
 import React from 'react'
+import { notFound } from 'next/navigation'
 
 export default function ReviewDetails({ params: { productId, reviewId } }: { params: { productId: string; reviewId: string; } }) {
+    if (parseInt(reviewId) > 100) {
+        notFound();
+    }
     return (
         <div>Review {reviewId} for Product {productId}</div>
     )
