7568e92dcecc47e1587ff657d2e951185267c593
ahsan-ahmed <ahsanahmed0124@gmail.com>
2024-11-01 21:42:05 +0500

    Error handling by adding the error.tsx file and loading indicator by add the file loading.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div style={{ height: '100%' }}>
       <h1>Home page</h1>
+
+      <ul>
+        <li>
+          <Link href={"/about"}>About page</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog page</Link>
+        </li>
+        <li>
+          <Link href={"/dashboard"}>Dashboard</Link>
+        </li>
+        <li>
+          <Link href={"/docs"}>Docs</Link>
+        </li>
+        <li>
+          <Link href={"/products"}>Product</Link>
+        </li>
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -3,7 +3,14 @@
 import React from 'react'
 import { notFound } from 'next/navigation'
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count)
+}
+
 export default function ReviewDetails({ params: { productId, reviewId } }: { params: { productId: string; reviewId: string; } }) {
+    if (getRandomInt(2) === 1) {
+        throw new Error(`Error loading review`)
+    }
     if (parseInt(reviewId) > 100) {
         notFound();
     }
