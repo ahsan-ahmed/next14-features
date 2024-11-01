f57687be5f5bb7667f7d0d04d56c2ce179e01ab7
ahsan-ahmed <ahsanahmed0124@gmail.com>
2024-11-01 21:11:18 +0500

    using Link instead of anchor tags

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,16 @@
 // Dynamic Routes
 
 import React from 'react'
+import Link from "next/link"
 
 export default function Products() {
   return (
     <>
       <div>Products</div>
       <ul>
-        <li><a href='products/1'>Product 1</a></li>
-        <li><a href='products/2'>Product 2</a></li>
-        <li><a href='products/3'>Product 3</a></li>
+        <li><Link href='products/1'>Product 1</Link></li>
+        <li><Link href='products/2'>Product 2</Link></li>
+        <li><Link href='products/3' replace>Product 3</Link></li>
       </ul>
     </>
   )
