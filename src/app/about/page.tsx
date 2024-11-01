f7de11e08248f24e2e1c5f14ca841c601ed35760
ahsan-ahmed <ahsanahmed0124@gmail.com>
2024-11-01 20:09:46 +0500

    generated metadata function and object and dynamic routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,12 @@
 // Multiple Routes
 
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "About title",
+  description: "About description",
+};
 
 export default function About() {
   return (
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,21 @@
 // Dynamic Routes
 
+import { Metadata } from 'next';
 import React from 'react'
 
-export default function Product({ params: { productId } }: { params: { productId: string } }) {
+type Props = { params: { productId: string } }
+
+export const generateMetadata = async ({ params: { productId } }: Props): Promise<Metadata> => {
+  const title = await new Promise<string>((resolve) => {
+    setTimeout(() => resolve(productId), 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  }
+};
+
+export default function Product({ params: { productId } }: Props) {
   return (
     <div>Product {productId}</div>
   )
